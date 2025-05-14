a4af425de898cea39efe550dce26b00ba6dc664e
ShadesAndGrays <cto.shadowcat@gmail.com>
2025-05-14 21:00:08 +0100

    refactor

---
+++ dapp/index.js
@@ -0,0 +1,7 @@
+GetCountBtn = document.getElementById("getCountBtn");
+CountInput = document.getElementById("countInput");
+
+
+GetCountBtn.addEventListener("click", () => {
+   CountInput.value = "213123"
+})
