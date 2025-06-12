8c10aaa48e75a1769ff2a508ad07a63976c457af
ShadesAndGrays <cto.shadowcat@gmail.com>
2025-06-12 17:25:15 +0100

    feat: client can register using smart contract

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -2,80 +2,117 @@ import { useEffect, useState } from "react";
 import { Web3, Contract } from "web3";
 
 import "./App.css";
+import RegistrationArtifact from "./Contracts/Registration.json";
 
 function App() {
-  const [input, setInput] = useState(1)
+  const [input, setInput] = useState(1);
   const [count, setCount] = useState(1);
-	const [warning, setWarning] = useState<string | null>(null);
-	const [web3, setWeb3] = useState<Web3 | null>(null);
-  const [connectButtonDisabled, setConnectButtonDisabled] = useState<boolean> (false);
-	const [accounts, setAccounts] = useState<string[] | null>(null);
-	const [connectedAccount, setConnectedAccount] = useState<string | null>(null);
-	const [contract, setContract] = useState< Contract<any> | null>(null);
+  const [warning, setWarning] = useState<string | null>(null);
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [connectButtonDisabled, setConnectButtonDisabled] =
+    useState<boolean>(false);
+  const [accounts, setAccounts] = useState<string[] | null>(null);
+  const [connectedAccount, setConnectedAccount] = useState<string | null>(null);
+  const [contract, setContract] = useState<Contract<any> | null>(null);
 
-
-  contract
-  setContract
+  contract;
+  setContract;
   // Set up web3
-	useEffect(() => {
-		// ensure that there is an injected the Ethereum provider
-		if (window.ethereum) {
-			// use the injected Ethereum provider to initialize Web3.js
-      setWeb3(new Web3(window.ethereum))
+  useEffect(() => {
+    // ensure that there is an injected the Ethereum provider
+    if (window.ethereum) {
+      // use the injected Ethereum provider to initialize Web3.js
+      setWeb3(new Web3(window.ethereum));
       setConnectButtonDisabled(false);
-
-    }else{
-			// no Ethereum provider - instruct user to install MetaMask
-			setWarning('Please install MetaMask');
+    } else {
+      // no Ethereum provider - instruct user to install MetaMask
+      setWarning("Please install MetaMask");
       setConnectButtonDisabled(true);
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
-
-    const fs = require('fs')
-    const path = require('path')
-
-    const deployed_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    const abi = require('./Contracts/Registration.json')
-    if (web3){
-      const c = new web3.eth.Contract(abi,deployed_address)
-      c.handleRevert = true
-      setContract(c)
+    const deployed_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const abi = RegistrationArtifact.abi;
+    console.log(abi);
+    if (web3) {
+      const c = new web3.eth.Contract(abi, deployed_address);
+      c.handleRevert = true;
+      setContract(c);
     }
-  },[web3])
+  }, [web3]);
 
-  // Example function of interacting with local python flask server 
-   let getFactorial = () =>{
+  // Example function of interacting with local python flask server
+  let getFactorial = () => {
     fetch(`http://localhost:5000/calculate?number=${input}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setCount(Number(data.result));
-            })
-        .catch(error => console.error('Error:', error));
-}
-let requestAccounts = async () =>  {
-  if (web3 === null){
-    console.log("Web3 is not initialzed")
-    return;
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCount(Number(data.result));
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+  let requestAccounts = async () => {
+    if (web3 === null) {
+      console.log("Web3 is not initialzed");
+      return;
+    }
 
-	await window.ethereum.request({ method: 'eth_requestAccounts' });
-  const allAccounts = await web3.eth.getAccounts();
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    const allAccounts = await web3.eth.getAccounts();
 
-  setAccounts(allAccounts);
-  setConnectedAccount(allAccounts[0]);
-  console.log("Web3 is initialized Account: ", allAccounts[0] ,allAccounts?.length)
-}
+    setAccounts(allAccounts);
+    setConnectedAccount(allAccounts[0]);
+    console.log(
+      "Web3 is initialized Account: ",
+      allAccounts[0],
+      allAccounts?.length
+    );
+  };
+
+  const handleRegister = async () => {
+    if (!contract || !connectedAccount) {
+      setWarning("Contract / Account is not initialized");
+      return;
+    }
+    try {
+      const tx = await contract.methods
+        .register(connectedAccount)
+        .send({ from: connectedAccount });
+
+      console.log("Transaction successful:", tx);
+      setWarning("Registration successful!");
+    } catch (error) {
+      console.error("Transaction failed", error);
+      setWarning("Transaction failed: " + (error as Error).message);
+    }
+  };
   return (
     <>
       <div>
-        <div id="account">{connectedAccount ? `Address:  ${connectedAccount}` : ""}</div>
+        <div id="account">
+          {connectedAccount ? `Address:  ${connectedAccount}` : ""}
+        </div>
         <h1> Federated Day </h1>
-        <div id="warn" style={{color: 'red'}}> {warning} </div>
-        <p>Register to participate in the Blockchain Federated Learning Process</p>
-        <button onClick={() => requestAccounts()} disabled={connectButtonDisabled}> Connect Wallet and Register</button>
+        <div id="warn" style={{ color: "red" }}>
+          {warning}
+        </div>
+        <p>
+          Register to participate in the Blockchain Federated Learning Process
+        </p>
+        <button
+          onClick={() => requestAccounts()}
+          disabled={connectButtonDisabled}
+        >
+          {" "}
+          Connect Wallet
+        </button>
+        <button
+          onClick={handleRegister}
+          disabled={!connectedAccount || !contract}
+        >
+          Register on Blockchain
+        </button>
       </div>
     </>
   );
--- smart-contract-dev/scripts/register.js
+++ smart-contract-dev/scripts/register.js
@@ -4,10 +4,11 @@ const hre = require("hardhat");
 async function main() {
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const myContract = await hre.ethers.getContractAt("Registration", contractAddress);
+  const account = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
   // const txResult = await myContract.register("0xE1f69F3ca3D2983cb622458b405024daA934aa24");
   // console.log(txResult);
-
-  const result = await myContract.isValid("0xE1f69F3ca3D2983cb622458b405024daA934aa24");
+  const result = await myContract.isValid(account)
+  // const result = await myContract.isValid("0xE1f69F3ca3D2983cb622458b405024daA934aa24");
   // const result = await myContract.getOwner();
   console.log(result);
 
