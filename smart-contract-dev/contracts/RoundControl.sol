82d4866fdb1a25f1d0016edab599627860d46e85
ShadesAndGrays <cto.shadowcat@gmail.com>
2025-06-12 20:08:56 +0100

    feat: added basic round control contract

---
+++ smart-contract-dev/contracts/RoundControl.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Round {
+    uint256 public roundId;
+    string public globalModelCid;
+    string public nextRoundCid;
+    uint64 public noOfParticipants;
+    address[] private participants;
+    mapping(address => string) participantCid;
+
+    constructor(
+        uint256 _roundId,
+        uint64 _noOfParticipants,
+        string memory _globalModelCid
+    ) {
+        roundId = _roundId;
+        globalModelCid = _globalModelCid;
+        noOfParticipants = _noOfParticipants;
+    }
+
+    // Remove this later in favour of register contract 
+    mapping(address => bool) public isParticipant;
+
+    function addParticipant(address participant) public {
+        require(!isParticipant[participant], "Failed AddParticipant: Participant is already registered");
+        participants.push(participant);
+        isParticipant[participant] = true;
+    }
+
+    function isRegistered(address participant) public view returns (bool) {
+        return isParticipant[participant];
+    }
+
+    function getCids() public view returns (string[] memory) {
+        string[] memory cids = new string[](participants.length);
+        for (uint i = 0; i < participants.length; i++) {
+            cids[i] = participantCid[participants[i]];
+        }
+        return cids;
+    }
+
+    function Aggregate(string memory _nextRoundCid) public {
+        nextRoundCid = _nextRoundCid;
+    }
+
+    function submitModelCid(address participant, string memory cid) public {
+        require(isParticipant[participant],"Failed Submit: participant is not registered");
+        participantCid[participant] = cid;
+    }
+}
+
+contract RoundControl {
+    uint256 private currentRoundId;
+    bool private active;
+    address public owner;
+    string private initialGlobalModelCid;
+
+    mapping(uint256 => address) roundMapping;
+
+    event RoundStarted(uint256 roundId);
+    event RoundEnded(uint256 roundId);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not contract owner");
+        _;
+    }
+
+    constructor(string memory _initialGlobalModelCid) {
+        active = false;
+        currentRoundId = 0;
+        owner = msg.sender;
+        initialGlobalModelCid = _initialGlobalModelCid;
+    }
+
+    function getRound(uint256 _roundId) public view returns (address) {
+        return roundMapping[_roundId];
+    }
+
+    function createRound(uint64 _noOfParticipants) internal returns (uint256) {
+        require(active, "Failed Create:  Current Round is not active");
+
+        uint256 roundId = currentRoundId;
+
+        if (roundId == 0) {
+            // First Round
+            Round newRound = new Round(
+                currentRoundId,
+                _noOfParticipants,
+                initialGlobalModelCid
+            );
+            roundMapping[currentRoundId] = address(newRound);
+        } else {
+            Round newRound = new Round(
+                currentRoundId,
+                _noOfParticipants,
+                Round(roundMapping[roundId - 1]).nextRoundCid()
+            );
+            roundMapping[currentRoundId] = address(newRound);
+        }
+        return roundId;
+    }
+
+    function nextRound() private {
+        currentRoundId = currentRoundId + 1;
+    }
+
+    function endRound() public onlyOwner {
+        require(active, "Failed end: No active round currently in progress");
+        active = false;
+        emit RoundEnded(currentRoundId);
+        nextRound();
+    }
+
+    function startRound(
+        uint64 _noOfParticipants
+    ) public onlyOwner returns (uint256) {
+        require(!active, "Failed Start: Round is currently running");
+        active = true;
+        emit RoundStarted(currentRoundId);
+        createRound(_noOfParticipants);
+        return currentRoundId;
+    }
+
+    function submitParticipantCid(string memory cid) public {
+        require(active, "No active round");
+        Round(roundMapping[currentRoundId]).submitModelCid(msg.sender, cid);
+    }
+}
